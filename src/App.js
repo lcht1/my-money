e45a7dfeb84b36fc49f6c10474add10aa5745b2f
lcht1 <manuleichtveis@gmail.com>
2022-09-20 14:02:20 -0300

    fix: undefined amount bug

--- src/App.js
+++ src/App.js
@@ -7,26 +7,28 @@ import Form from "./components/Form";
 const App = () => {
     const data = localStorage.getItem("transactions");
 
-    const [transactionsList, setTransactionsList] = useState(JSON.parse(data));
+    const [transactionsList, setTransactionsList] = useState(
+        data ? JSON.parse(data) : []
+    );
     const [income, setIncome] = useState(0);
     const [expense, setExpense] = useState(0);
     const [total, setTotal] = useState(0);
 
     useEffect(() => {
         const amountExpense = transactionsList
-            .filter((item) => item.expense)
-            .map((transaction) => Number(transaction.amount));
+            ?.filter((item) => item.expense)
+            ?.map((transaction) => Number(transaction.amount));
 
         const amountIncome = transactionsList
-            .filter((item) => !item.expense)
-            .map((transaction) => Number(transaction.amount));
+            ?.filter((item) => !item.expense)
+            ?.map((transaction) => Number(transaction.amount));
 
         const expense = amountExpense
-            .reduce((acc, cur) => acc + cur, 0)
-            .toFixed(2);
+            ?.reduce((acc, cur) => acc + cur, 0)
+            ?.toFixed(2);
         const income = amountIncome
-            .reduce((acc, cur) => acc + cur, 0)
-            .toFixed(2);
+            ?.reduce((acc, cur) => acc + cur, 0)
+            ?.toFixed(2);
 
         const total = Math.abs(income - expense).toFixed(2);
 
